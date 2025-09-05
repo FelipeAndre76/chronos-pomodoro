f535c72763da15f4433534d1b01e70ee7551b300
Felipe Andre <felipeandre76@hotmail.com>
2025-09-04 22:43:55 -0300

    Troca de Tema para Dark ou Light

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { PlayCircleIcon } from 'lucide-react';
 import { Container } from './components/Container';
 import { CountDown } from './components/CountDown';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
 import { DefaultInput } from './components/DefaultInput';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { Footer } from './components/Footer';
+
 
 import './styles/global.css';
 import './styles/theme.css';
 
+
 export function App() {
+
+
     return (
       <>
           <Container>
@@ -33,14 +41,17 @@ export function App() {
             </div>
 
             <div className="formRow">
-              <p>Ciclos</p>
-              <p>0 0 0 0 0</p>
+             <Cycles />
             </div>
 
             <div className="formRow">
-              <button>Enviar</button>
+              <DefaultButton icon={<PlayCircleIcon />}/>
             </div>
            </form>
+          </Container>
+
+           <Container>
+           <Footer />
           </Container>
       </>
      
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,23 +1,50 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useState, useEffect } from 'react';
 
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+
+    const [theme, setTheme] = useState<AvailableThemes>(() => {
+        const storageTheme = localStorage.getItem('theme') as AvailableThemes || 'dark';
+        return storageTheme;
+    });
+
+    const nextThemeIcom = {
+        dark: <SunIcon />,
+        light: <MoonIcon />
+    };
+
+    function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+        event.preventDefault();
+
+        setTheme(prevTheme => {
+            const newTheme = prevTheme === 'dark' ? 'light' : 'dark';
+            return newTheme;
+        });
+    }
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    },[theme]);
 
     return (
     
         <nav className={styles.menu}>
-            <a className={styles.MenuLink} href="#">
+            <a className={styles.MenuLink} href="#" aria-label='Pagina inicial' title='Home'>
                 <HouseIcon />
             </a>
-             <a className={styles.MenuLink} href="#">
+             <a className={styles.MenuLink} href="#" aria-label='Ver Histórico' title='Histórico'>
                 <HistoryIcon />
             </a>
-             <a className={styles.MenuLink} href="#">
+             <a className={styles.MenuLink} href="#" aria-label='Configurações' title='Configurações'>
                 <SettingsIcon />
             </a>
-             <a className={styles.MenuLink} href="#">
-                <SunIcon />
+             <a className={styles.MenuLink} href="#" aria-label='Mudar o Tema' title='Mudar o Tema' onClick={handleThemeChange}>
+              {nextThemeIcom[theme]}
             </a>
         </nav>
     );
